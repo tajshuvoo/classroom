07e88bd61d7c9752f7fa47a0e36fc8030aa92ec6
tajshuvo <tajshuvo75@gmail.com>
2023-08-05 18:17:57 +0600

    8th draft

--- app/src/main/java/com/example/classroom/JoinClassroomActivity.java
+++ app/src/main/java/com/example/classroom/JoinClassroomActivity.java
@@ -1,14 +1,138 @@
 package com.example.classroom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class JoinClassroomActivity extends AppCompatActivity {
 
+    private EditText classCode;
+    private Button buttonJoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_classroom);
+
+        //status bar color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.white));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                View decor = getWindow().getDecorView();
+                decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        }
+
+        // Toolbar setup
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        toolbar.setContentInsetsAbsolute(0, 0);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Join Class");
+
+
+        // Initialize views
+        classCode = findViewById(R.id.joinClassroom);
+        buttonJoin = findViewById(R.id.buttonJoin);
+
+
+        //user id
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String userId = sharedPreferences.getString("userId", null);
+
+
+        //Click event handling
+        buttonJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String classroomCode = classCode.getText().toString().trim();
+                if (!classroomCode.isEmpty()) {
+                    DatabaseReference linkedClassesRef = FirebaseDatabase.getInstance("https://classroom-adefd-default-rtdb.asia-southeast1.firebasedatabase.app")
+                            .getReference().child("linked_classes").child(userId).child("classrooms");
+
+                    // Check if the classroom with the given code exists
+                    DatabaseReference classroomsRef = FirebaseDatabase.getInstance("https://classroom-adefd-default-rtdb.asia-southeast1.firebasedatabase.app")
+                            .getReference().child("classrooms");
+                    classroomsRef.orderByChild("classroomId").equalTo(classroomCode).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                // Classroom with the provided code exists
+
+                                // Check if the user is already in the classroom
+                                linkedClassesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.hasChild(classroomCode)) {
+                                            // User is already in the classroom
+                                            Toast.makeText(JoinClassroomActivity.this, "You are already a member of this class.", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            // Add the classroom code to the user's linked_classes node
+                                            linkedClassesRef.child(classroomCode).setValue(classroomCode)
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful()) {
+                                                                Toast.makeText(JoinClassroomActivity.this, "Joined class successfully!", Toast.LENGTH_SHORT).show();
+                                                                finish();
+                                                            } else {
+                                                                Toast.makeText(JoinClassroomActivity.this, "Failed to join class. Please try again.", Toast.LENGTH_SHORT).show();
+                                                            }
+                                                        }
+                                                    });
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                                        // Handle errors
+                                    }
+                                });
+                            } else {
+                                // Classroom with the provided code does not exist
+                                Toast.makeText(JoinClassroomActivity.this, "Classroom with this code does not exist.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Handle errors
+                        }
+                    });
+                }
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
